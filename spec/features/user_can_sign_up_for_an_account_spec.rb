38a840c6da0d096e1178e5078454a131f976f904
flis_ann <ann.flismark@gmail.com>
2020-11-11 21:09:21 +0100

    Commiting a big mess that Thomas will have a look at

--- spec/features/user_can_sign_up_for_an_account_spec.rb
+++ spec/features/user_can_sign_up_for_an_account_spec.rb
@@ -9,6 +9,7 @@ feature 'User can sign up for an account' do
       expect(page).to have_content 'Sign up'
       expect(page).to have_field 'Name'
       expect(page).to have_field 'Email'
+      expect(page).to have_text '(8 characters minimum)'
       expect(page).to have_field 'Password'
       expect(page).to have_field 'Password confirmation'
     end
@@ -16,6 +17,19 @@ feature 'User can sign up for an account' do
     it 'display Create button' do
       expect(page).to have_button 'Create'
     end
+
+    it 'User should see success message' do
+      fill_in 'Name', with: 'user712'
+      fill_in 'Email', with: 'user712@test.com'
+      fill_in 'Password', with: '12345678'
+      fill_in 'Password confirmation', with: '12345678'
+      click_on 'Create'
+      expect(page).to have_content "Welcome! You have signed up successfully."
+    end
+
+    # it 'user enters her name' do 
+    #   expect(page) has_field? 'Name', with: 'Jane Fonda'
+    # end
 
   end
 end
