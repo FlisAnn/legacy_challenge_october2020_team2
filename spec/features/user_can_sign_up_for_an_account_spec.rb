e972eeff9eb5d51b139614445b247f6994cba9c6
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-11 16:13:50 +0100

    Adding more tests in user can create an account

--- spec/features/user_can_sign_up_for_an_account_spec.rb
+++ spec/features/user_can_sign_up_for_an_account_spec.rb
@@ -2,10 +2,20 @@ feature 'User can sign up for an account' do
   context 'with sign-up form' do
     before do
       visit root_path
+      click_on 'Sign up'
     end
 
     it 'displays the sign-up form' do
       expect(page).to have_content 'Sign up'
+      expect(page).to have_field 'Name'
+      expect(page).to have_field 'Email'
+      expect(page).to have_field 'Password'
+      expect(page).to have_field 'Password confirmation'
     end
+
+    it 'display Create button' do
+      expect(page).to have_button 'Create'
+    end
+
   end
 end
