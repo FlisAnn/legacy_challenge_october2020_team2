05d48ca54478671a145100c70889d9a6968172cf
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-12 20:26:24 +0100

    Refactoring code in user can sign up an account

--- spec/features/user_can_sign_up_for_an_account_spec.rb
+++ spec/features/user_can_sign_up_for_an_account_spec.rb
@@ -5,37 +5,42 @@ feature 'User can sign up for an account' do
       click_on 'Sign up'
     end
 
-    it 'displays the sign-up form' do
-      expect(page).to have_content 'Sign up'
-      expect(page).to have_field 'Name'
-      expect(page).to have_field 'Email'
-      expect(page).to have_field 'Password'
-      expect(page).to have_field 'Password confirmation'
-    end
-  end
-
-  context 'when user signs up' do
-    before do
-      visit root_path
-      click_on 'Sign up'
-    end
-
-    it 'is expected user to fill in the form' do
+    it 'is expected user to fill in the sign up form' do
       fill_in 'Name', with: 'Nicole'
       fill_in 'Email', with: 'baby@gmail.com'
       fill_in 'Password', with: '87653229'
-      fill_in 'user_password_confirmation', with: '87653229'
+      fill_in 'Password confirmation', with: '87653229'
       click_on 'Create'
       expect(page).to have_content "Welcome! You have signed up successfully."
     end
 
-    it 'alerts an error message if each field is not completed correctly' do
+    it 'is expected user to fill in the name' do
+      fill_in 'Name', with: ''
       click_on 'Create'
       expect(page).to have_content 'Name can\'t be blank'
-      expect(page).to have_content 'Email can\'t be blank'
-      expect(page).to have_content 'Password can\'t be blank'
-      expect(page).to have_content 'Password confirmation'
-      expect(page).to have_content '8 characters minimum'
     end
+
+    it 'is expected user to fill in the email' do
+      fill_in 'Email', with: ''
+      click_on 'Create'
+      expect(page).to have_content 'Email can\'t be blank'
+    end
+
+    it 'is expected user to fill in the password' do
+      fill_in 'Password', with: ''
+      click_on 'Create'
+      expect(page).to have_content 'Password can\'t be blank'
+    end
+
+    it 'is expected user to fill in the password confirmation' do
+      fill_in 'Password confirmation', with: ''
+      click_on 'Create'
+      expect(page).to have_content 'Password confirmation'
+    end
+
+    it 'is expected user to fill in 8 characters minimum password ' do
+      click_on 'Create'
+      expect(page).to have_content '8 characters minimum'
+    end 
   end
 end
