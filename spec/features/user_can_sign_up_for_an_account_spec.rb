05733becaf7375939f60e85aaf4f4d85ed6d87b1
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-12 16:28:45 +0100

    Adding sad path tests in user can create an account

--- spec/features/user_can_sign_up_for_an_account_spec.rb
+++ spec/features/user_can_sign_up_for_an_account_spec.rb
@@ -12,10 +12,6 @@ feature 'User can sign up for an account' do
       expect(page).to have_field 'Password'
       expect(page).to have_field 'Password confirmation'
     end
-
-    it 'display Create button' do
-      expect(page).to have_button 'Create'
-    end
   end
 
   context 'when user signs up' do
@@ -23,12 +19,23 @@ feature 'User can sign up for an account' do
       visit root_path
       click_on 'Sign up'
     end
+
     it 'is expected user to fill in the form' do
       fill_in 'Name', with: 'Nicole'
       fill_in 'Email', with: 'baby@gmail.com'
       fill_in 'Password', with: '87653229'
-      fill_in 'Password confirmation', with: '87653229'
+      fill_in 'user_password_confirmation', with: '87653229'
       click_on 'Create'
+      expect(page).to have_content "Welcome! You have signed up successfully."
+    end
+
+    it 'alerts an error message if each field is not completed correctly' do
+      click_on 'Create'
+      expect(page).to have_content 'Name can\'t be blank'
+      expect(page).to have_content 'Email can\'t be blank'
+      expect(page).to have_content 'Password can\'t be blank'
+      expect(page).to have_content 'Password confirmation'
+      expect(page).to have_content '8 characters minimum'
     end
   end
 end
